9c3ab01353048f172b68e7687021be7092891f1e
Bryan Workman <bryan@bryans-mbp.lan>
2020-10-29 13:50:05 -0700

    Views: Transaction List View

---
+++ AmartPhoto/Views/CustomViews/ProjectsTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ProjectsTableViewCell.swift
+//  AmartPhoto
+//
+//  Created by Bryan Workman on 10/29/20.
+//
+
+import UIKit
+
+class ProjectsTableViewCell: UITableViewCell {
+
+    //MARK: - Outlets
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    //MARK: - Properties
+    var myString: String? {
+        didSet {
+            updateViews()
+        }
+    }
+    
+    //MARK: - Helper Methods
+    func updateViews() {
+        guard let myString = myString else {return}
+        addressLabel.text = myString
+    }
+
+} //End of class
