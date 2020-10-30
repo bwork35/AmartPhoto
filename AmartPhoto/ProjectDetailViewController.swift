3d3be63f4179ae523909f64dd52db3bc4fc7d1e2
Bryan Workman <bryan@bryans-mbp.lan>
2020-10-30 14:19:58 -0700

    Views: Create Transaction View

---
+++ AmartPhoto/ProjectDetailViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ProjectDetailViewController.swift
+//  AmartPhoto
+//
+//  Created by Bryan Workman on 10/29/20.
+//
+
+import UIKit
+
+class ProjectDetailViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var squareFeetLabel: UILabel!
+    @IBOutlet weak var vacantLabel: UILabel!
+    @IBOutlet weak var phoneNumberLabel: UILabel!
+    @IBOutlet weak var primaryDateLabel: UILabel!
+    @IBOutlet weak var secondaryDateLabel: UILabel!
+    @IBOutlet weak var projectDetailsLabel: UILabel!
+    @IBOutlet weak var notesLabel: UILabel!
+    
+    //MARK: - Properties
+    
+    
+    //MARK: - Lifecycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    //MARK: - Helper Methods
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+} //End of class
